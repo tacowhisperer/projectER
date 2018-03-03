7552be43046cb1d200f9b3efa4b3095c821cf666
tacowhisperer <chato94@gmail.com>
2018-03-03 03:29:06 -0600

    Refactor pdfjs -> download-file + pdf2json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const download = require('download-file');
 const http = require('http');
-const pdfjs = require('pdfjs-dist');
+
 const DINING_URL = "http://dining.rice.edu/undergraduate-dining/college-serveries/weekly-menus/";
 
 http.get(DINING_URL, response => {
@@ -21,7 +22,9 @@ http.get(DINING_URL, response => {
 			west: getPdfUrlOf("West", diningHTML)
 		};
 
-        	getPdfFromURL(pdfUrls.baker);
+        	savePdfToFile(pdfUrls, () => {
+
+        	});
 	});
 
 }).on("error", e => console.error("GET REQUEST ERROR: " + e.message));
@@ -41,58 +44,19 @@ function getPdfUrlOf(buildingName, diningHTML) {
 	}
 }
 
-function getPdfFromURL(url) {
-    pdfjs.workersrc = url;
-    pdfjs.getDocument(url).then(pdf => {
-        for (let i = 1; i <= pdf.pdfInfo.numPages; i++) {
-       	    const ii = i;
+function savePdfToFile(pdfUrls, callback) {
+	for (location in pdfUrls) {
+		let url = pdfUrls[location];
+		let options = {
+			directory: "./menu_pdfs",
+			filename: location + ".pdf"
+		};
 
-            Promise.all([getPdfPageTextPromise(ii, pdf)]).then(pageTextArray => {
-            	console.log("***Page " + ii + "***\n");
-
-            	for (var pageIdx = 0; pageIdx < pageTextArray.length; pageIdx++)
-            		console.log(pageTextArray[pageIdx]);
-
-            	console.log("\n***End Page " + ii + "***\n");
-
-            }).catch(e => {
-            	console.error("Fatal error reading pages");
-            	console.error(e.message);
-            });
-        }
-    }).catch(e => console.error(e.message));
-
-
-}
-
-/**
- * Retrieves the text of a specif page within a PDF Document obtained through pdf.js 
- * 
- * @param {Integer} pageNum Specifies the number of the page 
- * @param {PDFDocument} PDFDocumentInstance The PDF document obtained 
- *
- * credit: https://jsfiddle.net/ourcodeworld/9s3hpbu2/
- **/
-function getPdfPageTextPromise(pageNum, PDFDocumentInstance) {
-    // Return a Promise that is solved once the text of the page is retrieven
-    return new Promise(function (resolve, reject) {
-        PDFDocumentInstance.getPage(pageNum).then(function (pdfPage) {
-            // The main trick to obtain the text of the PDF page, use the getTextContent method
-            pdfPage.getTextContent().then(function (textContent) {
-                var textItems = textContent.items;
-                var finalString = "";
-
-                // Concatenate the string of the item to the final string
-                for (var i = 0; i < textItems.length; i++) {
-                    var item = textItems[i];
-                    const ii = i;
-
-                    finalString += "Text Item #" + ii + ":\n" + item.str + "\n\n";
-                }
-
-                // Solve promise with the text retrieven from the page
-                resolve(finalString);
-            });
-        });
-    });
+		download(url, options, e => {
+			if (e)
+				console.error("Error downloading PDF from '" + url + "'");
+			else 
+				console.log("Saved " + options.filename + " to file!");
+		});
+	}
 }
