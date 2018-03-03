3f1e868f7860035ff370dbf32d5fc49ea962b552
tacowhisperer <chato94@gmail.com>
2018-03-03 04:36:26 -0600

    Removed unnecessary information from pdf json.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const download = require('download-file');
+const fs = require('fs');
 const http = require('http');
+const PDFParser = require('pdf2json');
 
 const DINING_URL = "http://dining.rice.edu/undergraduate-dining/college-serveries/weekly-menus/";
+const MENU_FOLDER = "./menu_pdfs/";
 
 http.get(DINING_URL, response => {
 	let diningHTML = "";
@@ -22,9 +25,7 @@ http.get(DINING_URL, response => {
 			west: getPdfUrlOf("West", diningHTML)
 		};
 
-        	savePdfToFile(pdfUrls, () => {
-
-        	});
+        	savePdfToFile(pdfUrls, savePdfJsonToFile, Object.keys(pdfUrls), () => console.log("Success!"));
 	});
 
 }).on("error", e => console.error("GET REQUEST ERROR: " + e.message));
@@ -44,11 +45,11 @@ function getPdfUrlOf(buildingName, diningHTML) {
 	}
 }
 
-function savePdfToFile(pdfUrls, callback) {
+function savePdfToFile(pdfUrls, callback, arg0, arg1) {
 	for (location in pdfUrls) {
 		let url = pdfUrls[location];
 		let options = {
-			directory: "./menu_pdfs",
+			directory: MENU_FOLDER,
 			filename: location + ".pdf"
 		};
 
@@ -57,6 +58,45 @@ function savePdfToFile(pdfUrls, callback) {
 				console.error("Error downloading PDF from '" + url + "'");
 			else 
 				console.log("Saved " + options.filename + " to file!");
+
+			callback(arg0, arg1);
 		});
+	}
+}
+
+function savePdfJsonToFile(pdfs, callback) {
+	for (let i = 0; i < pdfs.length; i++) {
+		let pdfParser = new PDFParser();
+
+		pdfParser.on("pdfParser_dataError", e => {
+			console.error("Error parsing '" + pdfs[i] + "':");
+			console.error(e.parseError)
+		});
+
+		pdfParser.on("pdfParser_dataReady", rawPdfJsonObject => {
+			let pagesArray = rawPdfJsonObject.formImage.Pages;
+			for (let i = 0; i < pagesArray.length; i++) {
+				let pageInfoObj = pagesArray[i];
+				let newPageInfo = {};
+
+				newPageInfo.height = pageInfoObj.Height;
+				newPageInfo.texts = pageInfoObj.Texts;
+
+				for (let j = 0; j < newPageInfo.texts.length; j++)
+					newPageInfo.texts[j].R[0].T = decodeURIComponent(newPageInfo.texts[j].R[0].T);
+
+				pagesArray[i] = newPageInfo;
+			}
+
+			let pdfJsonObject = {
+				servery: pdfs[i],
+				numPages: pagesArray.length,
+				pages: pagesArray
+			};
+
+			fs.writeFile(MENU_FOLDER + pdfs[i] + ".json", JSON.stringify(pdfJsonObject, null, "\t"), callback)
+		});
+
+		pdfParser.loadPDF(MENU_FOLDER + pdfs[i] + ".pdf");
 	}
 }
